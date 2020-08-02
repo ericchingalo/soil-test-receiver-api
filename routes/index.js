83b5c8149ab7e82e724a84815b7747c108541f6f
chingaloEric <echingalo@gmail.com>
2020-08-02 14:58:17 +0300

    feat: 🎸 added handler for the soil test results

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.send('hello world!');
+/* Saving the results to firebase */
+router.get('/reults', (req, res, next) => {
+  (async () => {
+    const data = {
+      pH: Number(req.query.pH),
+      moisture: Number(req.query.moisture),
+      temperature: Number(req.query.temperature),
+    };
+
+    try {
+      return res.status(200).send({ message: 'created', data });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
 });
 
 module.exports = router;
