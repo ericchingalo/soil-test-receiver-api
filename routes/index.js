9c8b098ab3e6d38c7fe1baccf838036f6f7665cc
chingaloEric <echingalo@gmail.com>
2020-09-02 14:22:39 +0300

    fix: 🐛 returned user info upon saving soil results

--- routes/index.js
+++ routes/index.js
@@ -11,18 +11,53 @@ router.get('/results', (req, res, next) => {
       temperature: Number(req.query.temperature),
     };
 
-    const url = `https://us-central1-soil-test-api.cloudfunctions.net/app/api/save?pH=${data.pH}&moisture=${data.moisture}&temperature=${data.temperature}`;
+    const deviceId = req.query.device;
+
+    const resultPayload = {
+      device: deviceId,
+      results: [
+        {
+          parameter: 'pH',
+          value: data.pH,
+        },
+        {
+          parameter: 'moisture',
+          value: data.moisture,
+        },
+        {
+          parameter: 'temperature',
+          value: data.temperature,
+        },
+      ],
+    };
 
     try {
-      axios
-        .post(url, {})
-        .then((response) => {
-          return res.status(200).send({ message: 'created', data });
-        })
-        .catch((error) => {
-          return res.status(500).send(error);
-        });
+      const resultAPI = `https://chingalo.site/soil-analysis/api/results`;
+      const results = await axios.post(resultAPI, resultPayload);
+      const deviceDetails = await axios.get(
+        `https://chingalo.site/soil-analysis/api/devices/${deviceId}`,
+      );
+
+      const url = `https://us-central1-soil-test-api.cloudfunctions.net/app/api/save?pH=${
+        data.pH
+      }&moisture=${data.moisture}&temperature=${data.temperature}&user=${
+        deviceDetails.data.user.username || ''
+      }&location=${deviceDetails.data.user.region || ''}`;
+
+      try {
+        axios
+          .post(url, {})
+          .then((response) => {
+            return res.status(200).send({ message: 'created', data });
+          })
+          .catch((error) => {
+            return res.status(500).send(error);
+          });
+      } catch (error) {
+        return res.status(500).send(error);
+      }
     } catch (error) {
+      console.error(error);
       return res.status(500).send(error);
     }
   })();
