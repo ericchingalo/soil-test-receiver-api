dbeba2e0b6e0cf7b8479228bbbf735e28c471d71
chingaloEric <echingalo@gmail.com>
2020-09-02 14:22:39 +0300

    fix: 🐛 returned user info upon saving soil results

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,33 @@ router.get('/results', (req, res, next) => {
       temperature: Number(req.query.temperature),
     };
 
-    const url = `https://us-central1-soil-test-api.cloudfunctions.net/app/api/save?pH=${data.pH}&moisture=${data.moisture}&temperature=${data.temperature}`;
+    const deviceId = req.query.device;
+
+    const resultPayload = {
+      device: deviceId,
+      results: [
+        {
+          parameter: 'pH',
+          value: data.pH,
+        },
+        {
+          parameter: 'moisture',
+          value: data.moisture,
+        },
+        {
+          parameter: 'temperature',
+          value: data.temperature,
+        },
+      ],
+    };
+    const resultAPI = `https://chingalo.site/soil-analysis/api/results`;
+
+    const results = await axios.post(resultAPI, resultPayload);
+    const deviceDetails = await axios.get(
+      `https://chingalo.site/soil-analysis/api/devices/`,
+    );
+
+    const url = `https://us-central1-soil-test-api.cloudfunctions.net/app/api/save?pH=${data.pH}&moisture=${data.moisture}&temperature=${data.temperature}&user=${deviceDetails.user.username}$location=${deviceDetails.user.region}`;
 
     try {
       axios
